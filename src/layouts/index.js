6c859b38bd8e4dfb815cf232a28e85a383fa4f81
Elizabeth Keleshian <ekeleshian@gmail.com>
2018-06-10 14:16:11 -0700

    finally changed the index title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Data Science N00b
           </Link>
         </h1>
       )
