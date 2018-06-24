b088217a0dbefa787e33312d8cf2b0a2f9346b6a
Elizabeth Keleshian <ekeleshian@gmail.com>
2018-06-23 19:02:46 -0700

    fixed missing parameter to prismjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 800,
             },
           },
           {
@@ -30,7 +30,11 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          'gatsby-remark-prismjs',
+          {resolve:'gatsby-remark-prismjs', 
+          options:{
+ classPrefix: 'language-',
+}}
+            ,
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
         ],
